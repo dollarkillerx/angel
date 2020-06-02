391221ad6f2649097d4e71690c091ca94c93e442
dollarkiller <wangy@mvalley.io>
2020-06-02 19:29:29 +0800

    update main

--- main.go
+++ main.go
@@ -23,8 +23,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	io.Copy(os.Stdout, reader)
+	io.Copy(os.Stdout, reader)  // 获取容器拉取产生的输出到stdout
+	log.Println("容器拉取成功")
 
+	// 创建容器
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
 		Image: "alpine",
 		Cmd:   []string{"echo", "hello world"},
@@ -32,17 +34,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	log.Println("容器镜像创建成功")
 
+	// 执行容器
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		panic(err)
 	}
+	log.Println("容器执行")
 
+	// 等待容器运行结束
 	wait, err := cli.ContainerWait(ctx, resp.ID)
 	if err != nil {
 		panic(err)
 	}
-	log.Println(wait)
+	log.Println("容器运行结束 id: ",wait)
 
+	// 获取容器中产生的stdout
 	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
 	if err != nil {
 		panic(err)
